b15ebfd74c2be4da2290f61199f01344f90adc96
nicoproo777 <cortesnicolas097@gmail.com>
2026-02-09 13:42:48 -0500

    Add files via upload

---
+++ Corte____1/Practica_1/JavaApplication.java
@@ -0,0 +1,12 @@
+
+package javaapplication;
+
+public class JavaApplication {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World") ;
+       
+    }
+    
+}
